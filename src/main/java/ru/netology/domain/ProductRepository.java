251f4a83387fd596ea52769845e64f5a71c46cf1
qaelena181993 <85900418+qaelena181993@users.noreply.github.com>
2022-09-11 23:57:43 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/ProductRepository.java
@@ -0,0 +1,45 @@
+package ru.netology.domain;
+
+public class ProductRepository {
+    Product[] products = new Product[0];
+
+    public void save(Product product) {
+        Product[] tmp = new Product[products.length + 1];
+        for (int i = 0; i < products.length; i++) {
+            tmp[i] = products[i];
+        }
+        tmp[tmp.length - 1] = product;
+        products = tmp;
+    }
+
+    public Product[] getSavedProd() {
+
+        return products;
+    }
+
+    public void removeById(int id) {
+        if (findById(id) == null) {
+            throw new NotFoundException(
+                    "Element with id: " + id + " not found"
+            );
+        }
+        Product[] tmp = new Product[products.length - 1];
+        int copyToIndex = 0;
+        for (Product product : products) {
+            if (product.getId() != id) {
+                tmp[copyToIndex] = product;
+                copyToIndex++;
+            }
+        }
+        products = tmp;
+    }
+
+    public Product findById(int id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
+    }
+}
